ce8287cdec166b2ccaf23aed8cdffefbdc5503cc
zhangyingyun <zhang@qq.com>
2020-02-04 17:28:40 +0800

    2020-02-24蜀汇升级完成后，更新项目进度中的联系人

--- test.py
+++ test.py
@@ -1,41 +1,48 @@
-def dict_deal(dict_data):
-    temp = {}
-    for key, value in dict_data.items():
-        if isinstance(value, dict):
-            temp[key.lower()] = dict_deal(value)
-        elif isinstance(value, list):
-            temp[key.lower()] = deal_list(value)
-        else:
-            temp[key.lower()] = value
-    return temp
+from json import loads
+
+from utils.sqlutils import OPMysql
 
 
-def deal_list(data_list):
-    temp = []
-    for i, item in enumerate(data_list):
-        if isinstance(item, dict):
-            item = dict_deal(item)
-        elif isinstance(item, list):
-            item = deal_list(item)
-        else:
-            item = item
-        temp.append(item)
-    return temp
+class DealUser():
+
+    def __init__(self):
+        self.db = OPMysql()
+        self.all_persons = None
+
+    def get_all_Person(self):
+        query_sql = r"""select id, person,remark from tb_progress"""
+        result = self.db.query(query_sql)
+        for item in result:
+            item['person'] = loads(item['person'])
+        self.all_persons = result
+        print(self.all_persons)
+        self.deal()
+
+    def deal_person(self, person):
+        new_remark = []
+        if person['person']:
+            for oneperson in person['person']:
+                if oneperson['name'] == '' or oneperson['name'] is None:
+                    continue
+                new_person = r"""姓名：{name},电话:{phone},工种:{class},进场时间：{time},工资情况:{wage}""".format(**oneperson)
+                new_remark.append(new_person)
+        if person['remark'] != '' and person['remark'] != None:
+            new_remark.append(person['remark'])
+        return ';'.join(new_remark)
+
+    def update_data(self, id, msg):
+        update_sql = r"""
+            update tb_progress set remark='{}' where id={};
+        """.format(msg, id)
+        self.db.update(update_sql)
+
+    def deal(self):
+        for item in self.all_persons:
+            new_person = self.deal_person(item)
+            self.update_data(item['id'], new_person)
+            print('id:{} success!'.format(item['id']))
 
 
 if __name__ == '__main__':
-    dict_data = {
-        'A': 'a',
-        'B': ['a', {
-            'C': 12
-        }],
-        'D': {
-            "E": 'e',
-            "F": [1, 2,{
-                "G":123
-            }]
-        }
-    }
-    list_data = [dict_data] * 3
-    print(dict_deal(dict_data))
-    print(deal_list(list_data))
+    user = DealUser()
+    user.get_all_Person()
